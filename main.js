471ec268ef15d3d59dee827f3358b527054fa4df
Cara Ponzini <ponzini@gmail.com>
2019-01-17 19:45:53 -0800

    making greeting

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 //eslint-disable-next-line
 function greet(){
     
-    // get the first name input by id
-
-    // get the last name input by id
+    // get the first name and last name inputs by id
+    var firstNameInput = document.getElementById('first-name');
+    var lastNameInput = document.getElementById('last-name');
 
     // get values out of both inputs
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value; //value is an input type
+    console.log(firstName, lastName);
 
     // make greeting string
+    var greeting = 'Hello ' + firstName + " " +lastName + '!';
+    console.log(greeting);
 
     // display greeting
 
